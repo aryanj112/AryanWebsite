a120af34ee656b6b440fe107f315754f3f659587
Aryan Jain <2006aryanj@gmail.com>
2024-10-21 19:11:29 -0400

    added js so all three images' desc appears on hover

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ let desc1 = document.getElementById('desc1');
 image1.addEventListener('mouseover', function(){
     desc1.style.display = 'block';
 });
+image1.addEventListener('mouseout', function(){
+    desc1.style.display = 'none';
+});
 
 let image2 = document.getElementById('cave-photo');
-let image3 = document.getElementById('fort-photo');
+let desc2 = document.getElementById('desc2');
 
+image2.addEventListener('mouseover', function(){
+    desc2.style.display = 'block';
+});
+image2.addEventListener('mouseout', function(){
+    desc2.style.display = 'none';
+});
+
+let image3 = document.getElementById('fort-photo');
+let desc3 = document.getElementById('desc3');
+
+image3.addEventListener('mouseover', function(){
+    desc3.style.display = 'block';
+});
+image3.addEventListener('mouseout', function(){
+    desc3.style.display = 'none';
+});
