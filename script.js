637352277cd6f2364e22d6d038e5912f0a68359b
Aryan Jain <2006aryanj@gmail.com>
2024-10-21 19:06:44 -0400

    added js so first desc appears on hover

---
+++ script.js
@@ -0,0 +1,10 @@
+let image1 = document.getElementById('india-gate-photo');
+let desc1 = document.getElementById('desc1');
+
+image1.addEventListener('mouseover', function(){
+    desc1.style.display = 'block';
+});
+
+let image2 = document.getElementById('cave-photo');
+let image3 = document.getElementById('fort-photo');
+
